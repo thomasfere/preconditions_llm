8dcaea45de7c6d37af50c540d2eb8330f43cebe2
thomasfere <thomasferentinos@gmail.com>
2023-12-12 03:17:53 +0100

    initial

---
+++ main.py
@@ -0,0 +1,69 @@
+
+import requests
+import json
+import PyPDF2
+import openai
+import os
+from openai import OpenAI
+
+
+def extract_data_from_PDF():
+    file = "/Users/thomasferentinos/Downloads/FEK-2013-Tefxos A-00170-downloaded -11_12_2023.pdf"
+
+
+    with open(file, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        text = ''
+        for i in range(len(reader.pages)):
+            text += reader.pages[i].extract_text()
+
+    return text
+
+
+
+def extract_data_from_api():
+
+    response = requests.get('https://api.digigov.grnet.gr/v1/services/937099');
+    json_data = response.json()
+
+    total_preconditions = len(json_data["data"]["metadata"]["process_conditions"])
+
+    print("total preconditions are " + str(total_preconditions))
+
+    preconditions = []
+
+    for i in range(total_preconditions):
+        preconditions.append(json_data["data"]["metadata"]["process_conditions"][i]["conditions_name"])
+
+    return preconditions
+
+
+
+
+def call_llm(data):
+    os.environ['OPENAI_API_KEY'] = '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'
+    client = OpenAI()
+    prompt = f"list the preconditions from text {data} "
+
+    completion = client.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=prompt,
+        max_tokens=1000,
+        temperature=0
+    )
+
+    print(completion.choices[0].text)
+
+
+def main():
+
+    #data = extract_data_from_PDF()
+
+    data = extract_data_from_api()
+
+    print(data)
+
+    call_llm(data)
+
+
+main()
